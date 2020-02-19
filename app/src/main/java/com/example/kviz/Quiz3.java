4afc1b4ec48e59fc5affc98779eac7f64bcfcee2
osmanhadzic <hadzico39@gmail.com>
2020-02-19 16:04:11 +0100

    new quiz update with error

---
+++ app/src/main/java/com/example/kviz/Quiz3.java
@@ -0,0 +1,14 @@
+package com.example.kviz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Quiz3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz3);
+    }
+}
---
+++ app/src/main/java/com/example/kviz/Quiz6.java
@@ -0,0 +1,13 @@
+package com.example.kviz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Quiz6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
